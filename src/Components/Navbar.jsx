f88f090858c41ae05476c6a2be46dcb39e8f3df7
nzommmo <ericnzomo17@gmail.com>
2024-07-20 21:42:36 +0300

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,55 @@
-//import {Menu,X} from "lucide-react"
-import { useState } from "react"
+import React, { useState } from 'react'
+import { Menu, X } from 'lucide-react'
+
 const Navbar = () => {
-   
+    const [mobileDraweropen,setmobiledraweropen] = useState(false)
+
+    const toggleNavbar = () => {
+        setmobiledraweropen(!mobileDraweropen)
+    }
+
   return (
+    <nav className='sticky top-0-50 py-3 backdrop-blur-lg border-neutral-700/80'>
+        <div className='container px-4 mx-auto relative text-sm'>
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center flex-shrink'>
+                    <img src="" alt="logo" />
+                    <span className='text-xl tracking-tight'>CandleHut</span>
 
-<nav className="sticky top-0 z-50 backdrop-blur-lg rounded-md pt-2 w-25 ">
-    <div className="container px-4 mx-auto relative text-sm pb-3 ">
-        <div className="bg-customGreen flex items-center flex-shrink-0 rounded-md ">
-            <span className="text-xl tracking-tight">Candle Hut</span>
-            <ul className="flex md:flex-row md:flex flex-row items-center justify-center md:gap-20 space-x-20">
-            <li></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">About Us</a></li>
-        </ul>
-            
+                </div>
+                <ul className='hidden lg:flex ml-14 space-x-12'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+
+                </ul>
+                <div className='lg:hidden md:flex flex-col justify-end'>
+                    <button onClick={toggleNavbar} >
+                        {mobileDraweropen ? <X/> :<Menu/>}
+                    </button>
+
+
+                </div>
+
+            </div>
+            {mobileDraweropen &&(
+                <div className='fixed right-0-20 z-99 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden'>
+                     <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+
+                </ul>
+                
+
+                </div>
+            )}
+
         </div>
-       
-    </div>
-    
-</nav>
 
-)
+    </nav>
+    
+  )
 }
 
 export default Navbar
