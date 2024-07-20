b6a696e39a1997c7dfb888429cb8cce2947aaf38
nzommmo <ericnzomo17@gmail.com>
2024-07-20 22:18:08 +0300

    NavBar Update

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -2,11 +2,11 @@
 const HeroSection = () => {
   return (
     <div className="top-0 w-25 lg:w-1/2 border rounded-md border-customGreen ms-20 me-20 lg:ms-80">
-        <div className="container hover:tracking-widest   px-4 mx-auto relative flex-shrink-0 " >
-            <div className="text-xl text-left font-irishGrover font-extrabold">
+        <div className="container    px-4 mx-auto relative flex-shrink-0 " >
+            <div className="text-xl text-left font-inknutAntiqua font-extrabold">
                 <p>CANDLE HUT</p>
             </div>
-            <div className="text-customGreen text-xl  text-center font-islandMoments italic">
+            <div className="text-customGreen text-xl  text-center font-inknutAntiqua ">
                 <p>SCENTED CANDLES</p>
             </div>
             <div className="text-slate-200 text-right font-inknutAntiqua">
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,9 @@ const Navbar = () => {
     }
 
   return (
-    <nav className='sticky top-0-50 py-3 backdrop-blur-lg border-neutral-700/80'>
+    <div className='relative  z-50' >
+        
+    <nav className=' sticky top-0-50 py-3 backdrop-blur-lg border-neutral-700/80'>
         <div className='container px-4 mx-auto relative text-sm'>
             <div className='flex justify-between items-center'>
                 <div className='flex items-center flex-shrink'>
@@ -23,8 +25,8 @@ const Navbar = () => {
                     <li><a href="#">Contact Us</a></li>
 
                 </ul>
-                <div className='lg:hidden md:flex flex-col justify-end'>
-                    <button onClick={toggleNavbar} >
+                <div className='lg:hidden md:flex flex-coljustify-end'>
+                    <button onClick={toggleNavbar}>
                         {mobileDraweropen ? <X/> :<Menu/>}
                     </button>
 
@@ -33,8 +35,8 @@ const Navbar = () => {
 
             </div>
             {mobileDraweropen &&(
-                <div className='fixed right-0-20 z-99 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden'>
-                     <ul>
+                <div className='fixed right-0-20 bg-neutral-900 w-full pt-6 flex flex-col justify-center items-center lg:hidden pb-6'>
+                     <ul className='flex flex-col gap-2 '>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About Us</a></li>
                     <li><a href="#">Contact Us</a></li>
@@ -48,6 +50,8 @@ const Navbar = () => {
         </div>
 
     </nav>
+
+    </div>
     
   )
 }
