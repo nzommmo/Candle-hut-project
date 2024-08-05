20930c14f640b574b9d413541ef8b48eed6742f1
nzommmo <ericnzomo17@gmail.com>
2024-08-05 11:38:48 +0300

    Features

--- Frontend/Candlehut/src/Components/Homepage/Footer.jsx
+++ Frontend/Candlehut/src/Components/Homepage/Footer.jsx
@@ -1,14 +1,9 @@
-import CopyrightFooter from "../Products/CopyrightFooter";
 import { Facebook } from "lucide-react";
 import { Twitter} from "lucide-react";
 import { InstagramIcon } from "lucide-react";
 import { Building2Icon} from "lucide-react";
 import {CornerUpRight} from "lucide-react";
 import { Globe2Icon} from "lucide-react";
-
-
-
-
 
 const Footer = () => {
   return (
@@ -19,9 +14,9 @@ const Footer = () => {
             <hr className="w-full"/>
 
             <ul className="flex flex-col mt-2 gap-5 ">
-            <li><a href="#" className="flex gap-2 hover:text-customGreen"> <InstagramIcon/> Candle_Hut</a></li>
+            <li><a href="#" className="flex gap-2 hover:text-customGreen">  <InstagramIcon/> Candle_Hut</a></li>
             <li><a href="#" className="flex gap-2 hover:text-customGreen"><Facebook/> CandleHut_KE</a></li>
-            <li><a href="#" className="flex gap-2 hover:text-customGreen"> <Twitter/> CandleHut</a></li>
+            <li><a href="#" className="flex gap-2 hover:text-customGreen"><Twitter/> CandleHut</a></li>
             </ul>
 
           </div>
@@ -29,7 +24,7 @@ const Footer = () => {
             <h3 className="text-lg flex flex-row align-center justify-center">Location</h3>
             <hr className="w-full bg:neutral"/>
             <ul className="flex flex-col  mt-2 w-full gap-5">
-            <li><a href="#" className="hover:text-customGreen"> <Building2Icon/>Mshumaa House</a></li>
+            <li><a href="#" className="hover:text-white"><Building2Icon/>Mshumaa House</a></li>
             <li><a href="#" className="hover:text-customGreen"> <CornerUpRight/>Lenana Road - Off Waiyaki Way</a></li>
             <li><a href="#" className="hover:text-customGreen"><Globe2Icon/>Nairobi, Kenya</a></li>
             </ul>
@@ -39,22 +34,15 @@ const Footer = () => {
           <h3 className="text-lg flex flex-row align-center justify-center">Reviews</h3>
             <hr className="w-full"/>
 
-            <h2 className="mt-2 mb-2 flex align-center justify-center">Leave Us a Review</h2>
+            <h2 className="mt-2 mb-2">Leave Us a Review</h2>
             <textarea  className="rounded-md bg-white text-black"  name="" id="" cols="30" rows="5"></textarea>
             <div>
-            <button className="bg-customGreen float-right text-black px-5  my-2 rounded">Send</button>
+            <button className="bg-customGreen text-black px-5 float-right my-2 rounded">Send</button>
 
 
             </div>
-            
-
           </div>
-
-          
-          
         </div>
-        <CopyrightFooter/>
-
 
       </div>
   )
